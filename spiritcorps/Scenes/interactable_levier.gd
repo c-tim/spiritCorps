77110a4c1988696e2a71532065bce8ab83ed6777
c-tim <148889379+c-tim@users.noreply.github.com>
2025-03-22 14:49:07 +0100

    added levier

---
+++ spiritcorps/Scenes/interactable_levier.gd
@@ -0,0 +1,11 @@
+class_name interactable_levier extends AnimatedSprite2D
+
+signal levier_activated()
+
+func _ready() -> void:
+	play("levier_not_activated")
+
+func action_interract():
+	play("levier_not_activated")
+	levier_activated.emit()
+	
