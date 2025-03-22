2422bf66569701a0f1949f01728e4445786a8f04
owan.mannone <owan.mannone@telecom-sudparis.eu>
2025-03-22 21:17:08 +0100

    ajout du script levier du discord

--- spiritcorps/Scripts/levier.gd
+++ spiritcorps/Scripts/levier.gd
@@ -1,5 +1,16 @@
-class_name Levier extends Node2D
+#class_name Levier extends Node2D
 
+
+#func action_interract():
+	#pass
+	
+class_name interactable_levier extends AnimatedSprite2D
+
+signal levier_activated()
+
+func _ready() -> void:
+	play("levier_not_activated")
 
 func action_interract():
-	pass
+	play("levier_not_activated")
+	levier_activated.emit()
