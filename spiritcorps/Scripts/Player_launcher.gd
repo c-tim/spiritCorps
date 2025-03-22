44a04364593a927094c93e859510c00821d3362d
Célia D'Agostini <celidago57@gmail.com>
2025-03-22 10:30:18 +0100

    Première version du ralentissement si l'ame va trop loin

--- spiritcorps/Scripts/Player_launcher.gd
+++ spiritcorps/Scripts/Player_launcher.gd
@@ -4,24 +4,34 @@ extends Node2D
 @onready var ame: CharacterBody2D = $Ame
 
 var is_body=true
+var ralentissement=1.0
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	ame.visible=false
 	ame.position=corps.position
 	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Switch"):
 		ame.visible=!ame.visible
 		is_body=!is_body
 		switching(is_body)
+	limit_ame(ame.position.distance_to(corps.position))
 		
 func switching(is_body:bool) :
 	ame.is_moving(is_body)
 	corps.is_waiting(is_body)
 	if is_body:
 		ame.position=corps.position
+
+func limit_ame(dist:float):
+	if dist >= 500:
+		ralentissement= 1-((dist-500)/dist)*1.25
+		ame.eloignement(ralentissement)
+	if dist<500:
+		ame.eloignement(1)
+		
+
 	
 	
